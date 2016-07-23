c3b95da28994dec4a6fba6a623311f2c225eca7a
WANGYantong <WangytChina@outlook.com>
2016-07-23 15:20:15 +0800

    add power function

--- 10_sqrt_pow.c
+++ 10_sqrt_pow.c
@@ -9,15 +9,29 @@ int main(void)
 {
 	float num, precision = 0.001;
 	int i = 1;
-	int select,power;
+	int select, power;
 
 	while (i == 1) {
-		printf("\ninput a number:\n");
-		scanf("%f", &num);
-		printf("\ninput a power:\n");
-		scanf("%d", &power);
-		printf("\nsqrt:%f\n", mysqrt(num, precision));
-		printf("\npower:%f\n", mypow(num,power);
+		printf("\nwhat do you want to do?");
+		printf("\n1,sqrt\t2,power\n");
+		scanf("%d", &select);
+
+		switch (select) {
+		case (1):
+			printf("\ninput a number:\n");
+			scanf("%f", &num);
+			printf("\nsqrt:%f\n", mysqrt(num, precision));
+			break;
+		case (2):
+			printf("\ninput a number:\n");
+			scanf("%f", &num);
+			printf("\ninput a power:\n");
+			scanf("%d", &power);
+			printf("\npower:%f\n", mypow(num, power));
+			break;
+		default:
+			break;
+		}
 
 		printf("what do you want to do next?");
 		printf("\n1.go on\t2.stop\n");
@@ -48,18 +62,19 @@ float mysqrt(float y, float precision)
 	return mid;
 
 }
+
 float mypow(float x, int n)
 {
 	float result;
-	
-	if(n==0)
-		result=1;
-	if(n==1)
-		result=x;
-	else if(n%2==0)
-		result=mypow(x,n/2)*mypow(x,n/2);
+
+	if (n == 0)
+		result = 1;
+	if (n == 1)
+		result = x;
+	else if (n % 2 == 0)
+		result = mypow(x, n / 2) * mypow(x, n / 2);
 	else
-		result=mypow(x,n-1)*x;
+		result = mypow(x, n - 1) * x;
 
 	return result;
 }
